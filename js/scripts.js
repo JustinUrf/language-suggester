3be8e5922c89ff80ec2d33dd12a15feca64b52f4
Justin Lee <lee.justin001126@gmail.com>
2023-01-13 13:24:16 -0800

    remove prompts for name and age as they did not pertain to objectives

--- js/scripts.js
+++ js/scripts.js
@@ -1,24 +1,27 @@
+//Business Logic
 
+// function that takes in answer( which is avariable that converts user input into a number value and returns what langauge they should learn
 function sortEndResponse(answer) {
-  if (answer <= 5) {
-    return "Javascript";
-  } else if (answer > 5 && answer <= 10) {
-    return "Python";
-  } else if (answer > 10 && answer <= 15) {
-    return "HTML"
-  } else if (answer > 15 && answer <= 20) {
-    return "CSS"
+  if (answer >= 5 && answer <= 9) {
+    return "You choose a lot of A or B answers, the perfect language for you is Javascript!";
+  } else if (answer > 9 && answer <= 13) {
+    return "You choose a lot of B or C answers or a couple of D answers and a couple of A and B answers, the perfect langauge for you is Go!";
+  } else if (answer > 13 && answer <= 17) {
+    return "You choose a lot of C or B answers or a lot of D answers and a couple of B and A answers, the perfect language for you is Ruby!"
+  } else if (answer > 17 && answer <= 20) {
+    return "You choose a lot of C and D answers, the perfect langauge for you is C#!"
   }  
 }
-
-
+// This function converts user inputs into a final number to be compared to i.e 3 3 3 3 2 > returns 14. 
 function answerCalculation(userInput1, userInput2, userInput3, userInput4, userInput5){
   return userInput1 + userInput2 + userInput3 + userInput4 + userInput5
 }
 
-
-
 // UI logic
+
+// This function is called when the submit button gets intereacted with. This then gathers user input data which is assigned 
+// with a value 1-4, then adds them for final comparison for sortEndResponse). Finally, the text in ID results gets changed to 
+// whatever is returned from sortEndResponse
 function determineLangauge(event) {
   event.preventDefault();
   const userInput1 = parseInt(document.querySelector("input[name='use-types']:checked").value);
@@ -26,13 +29,11 @@ function determineLangauge(event) {
   const userInput3 = parseInt(document.querySelector("input[name='favourite-genre']:checked").value);
   const userInput4 = parseInt(document.querySelector("input[name='personality-type']:checked").value);
   const userInput5 = parseInt(document.querySelector("input[name='scenario']:checked").value);
-  console.log(userInput1, userInput2, userInput3, userInput4, userInput5)
-  let answer = answerCalculation(userInput1, userInput2, userInput3, userInput4, userInput5)
-  console.log(answer)
+  const answer = answerCalculation(userInput1, userInput2, userInput3, userInput4, userInput5)
   const result = sortEndResponse(answer);
-
-  document.getElementById("result").innerText = result
   
+  document.getElementById("result").innerText = result
+
 
 }
 
