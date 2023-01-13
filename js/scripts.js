52d2290c237e6e09f8fe749a7969c682ce4a60b6
Justin Lee <lee.justin001126@gmail.com>
2023-01-13 11:32:08 -0800

    add js logic to store all user answers as a number that will be used in determining language type

--- js/scripts.js
+++ js/scripts.js
@@ -3,12 +3,16 @@
 
 function determineLangauge(event) {
   event.preventDefault();
-  const userInput1 = document.querySelector("input[name='use-types']:checked").value;
-  const userInput2 = document.querySelector("input[name='favourite-activity']:checked").value;
-  const userInput3 = document.querySelector("input[name='favourite-genre']:checked").value;
-  const userInput4 = document.querySelector("input[name='personality-type']:checked").value;
-  const userInput5 = document.querySelector("input[name='scenario']:checked").value;
-  console.log(userInput1)
+  const userInput1 = parseInt(document.querySelector("input[name='use-types']:checked").value);
+  const userInput2 = parseInt(document.querySelector("input[name='favourite-activity']:checked").value);
+  const userInput3 = parseInt(document.querySelector("input[name='favourite-genre']:checked").value);
+  const userInput4 = parseInt(document.querySelector("input[name='personality-type']:checked").value);
+  const userInput5 = parseInt(document.querySelector("input[name='scenario']:checked").value);
+  console.log(userInput1, userInput2, userInput3, userInput4, userInput5)
+
+  const answer = userInput1 + userInput2 +userInput3 + userInput4 + userInput5
+  console.log(answer)
+
 }
 
 window.addEventListener("load", function() {
